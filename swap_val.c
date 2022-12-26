5f70501faa3f01f1703df777156add375b70f4de
AishwaryaJoshi087 <aishwaryajoshi762@gmail.com>
2022-12-26 11:21:07 +0000

    SWAP2

--- swap_val.c
+++ swap_val.c
@@ -6,13 +6,13 @@ int swap(int x,int y){
     x=y;
     y=temp;
     printf(" a=%d,b=%d\n",x,y);
-    //he;lo
+    
    
      }
 
 int main(){
     int a,b;
-    printf("enter two number:");
+    printf("Enter two number:");
     scanf("%d %d",&a ,&b);
     printf(" a=%d,b=%d\n",a,b);
     
